8c4bd743e8fb51f92aa10d233b4b6fd5b3e1efa4
Alexander Holovin <dvjmixxxer@gmail.com>
2017-07-13 11:10:50 +0300

    Move logging init to separate file

--- bin/main.py
+++ bin/main.py
@@ -1,33 +1,9 @@
-import logging
-import os
-from logging.handlers import RotatingFileHandler
-
-from config.config import Config
+from dev.logger import logger_setup
 from network.network import Network
 
 
-def logger_setup(mode):
-    if mode == 'dev':
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.INFO
-
-    log_formatter = logging.Formatter(Config.get('APP_LOG_FORMAT'), datefmt='%Y/%m/%d %H:%M:%S')
-    loggers = ['ddd_network']
-
-    handler = RotatingFileHandler('log.txt', maxBytes=10000000, backupCount=5)
-    handler.setLevel(log_level)
-    handler.setFormatter(log_formatter)
-
-    for logger_name in loggers:
-        logger = logging.getLogger(logger_name)
-        logger.addHandler(handler)
-
-    pass
-
-
 def main():
-    logger_setup(os.environ.get("SECRET_KEY"))
+    logger_setup([Network.logger_name])
     Network()
 
 
